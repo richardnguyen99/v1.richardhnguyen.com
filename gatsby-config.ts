02f7726daac6e8c0a49d492f5084efb04f4b41c9
Richard Nguyen <richard.ng0616@gmail.com>
2022-10-21 12:12:20 -0700

    Add gatsby plugin and content for manifest

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -18,7 +18,79 @@ const config: GatsbyConfig = {
     twitterUsername: "@richardmhnguyen",
     image: "/icon-512.png",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Richard H. Nguyen",
+        short_name: "richardhnguyen",
+        lang: "en",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#0f172a",
+        display: "standalone",
+        icon: "static/icon-512.png",
+        icons: [
+          {
+            src: "static/icon-gradient-16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: "static/icon-gradient-24.png",
+            sizes: "24x24",
+            type: "image/png",
+          },
+          {
+            src: "static/icon-gradient-32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "static/icon-gradient-48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "static/icon-gradient-64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "static/icon-gradient-128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "static/icon-gradient-256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "static/icon-gradient-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "static/icon-gradient-1024.png",
+            sizes: "1024x1024",
+            type: "image/png",
+          },
+        ],
+        localize: [
+          {
+            start_url: "/vi/",
+            lang: "vi",
+            name: "Blog của Richard",
+            short_name: "richardhnguyen",
+            description: "Blog cá nhân về công nghệ của Richard",
+          },
+        ],
+        crossOrigin: "use-credentials",
+      },
+    },
+  ],
 };
 
 export default config;
