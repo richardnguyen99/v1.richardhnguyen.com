86ea5ad308255efd5199fe8755dfbdd92dfe9ff1
Richard Nguyen <richard.ng0616@gmail.com>
2023-03-18 21:53:51 -0700

    Add manifest and favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -15,6 +15,7 @@ const config: GatsbyConfig = {
     siteUrl: "http://richardhnguyen.com",
   },
   plugins: [
+    // For working with images
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
@@ -29,6 +30,49 @@ const config: GatsbyConfig = {
         rule: {
           include: /assets/,
         },
+      },
+    },
+
+    // For web app manifest
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Richard H Nguyen's blog",
+        short_name: "richardhnguyen.com",
+        lang: "en",
+        start_url: "/",
+        background_color: "#18181b",
+        theme_color: "#818cf8",
+        display: "standalone",
+        crossOrigin: "use-credentials",
+        icon: "static/brand-16.png",
+        icons: [
+          {
+            src: "static/brand-16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: "static/brand-32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "static/brand-64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "static/brand-96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "static/brand-128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+        ],
       },
     },
   ],
