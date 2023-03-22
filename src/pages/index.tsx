6e408c723e06ee6b0fa74e213496d3f7c48289ee
Richard Nguyen <richard.ng0616@gmail.com>
2023-03-22 09:45:19 -0700

    Add:w

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
+import * as React from "react";
+import clsx from "classnames";
+
 import SwipeButton from "@components/Button/SwipeButton";
 import { SEO } from "@components/SEO";
 import TypeWriter from "@components/Typewritter";
-import * as React from "react";
 
 const IndexPage: React.FC = () => {
   return (
-    <div className="max-w-2xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl mx-auto py-12 md:py-16 lg:py-20 xl:py-24 px-10">
+    <div
+      className={clsx("py-12 px-6", {
+        "md:max-w-3xl md:py-16 md:px-10 md:mx-auto": true,
+        "lg:max-w-4xl lg:py-20": true,
+        "xl:max-w-6xl xl:py-24": true,
+      })}
+    >
       <TypeWriter />
       <div className="mt-12" />
       <SwipeButton />
