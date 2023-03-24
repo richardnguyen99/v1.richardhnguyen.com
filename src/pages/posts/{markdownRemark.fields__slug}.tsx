7c8aa646efb7caa27153eeee732e0bca2e5140f0
Richard Nguyen <richard.ng0616@gmail.com>
2023-03-24 11:44:26 -0700

    Refactored type for post template

--- src/pages/posts/{markdownRemark.fields__slug}.tsx
+++ src/pages/posts/{markdownRemark.fields__slug}.tsx
@@ -3,17 +3,16 @@ import { graphql, HeadFC, PageProps } from "gatsby";
 
 import { SEO } from "@components/SEO";
 
-const PostTemplate: React.FC<PageProps<Queries.PostTemplateQuery>> = ({
-  data,
-  children,
+type PostTemplateProps = Queries.PostTemplateQuery;
+
+const PostTemplate: React.FC<PageProps<PostTemplateProps>> = ({
+  data: _queriedData,
 }) => {
+  const data = _queriedData.markdownRemark;
+
   return (
-    <div>
-      <h1>PostTemplate</h1>
-      <div
-        id="content"
-        dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
-      />
+    <div id={data.id}>
+      <div id="content" dangerouslySetInnerHTML={{ __html: data.html }} />
     </div>
   );
 };
@@ -21,15 +20,31 @@ const PostTemplate: React.FC<PageProps<Queries.PostTemplateQuery>> = ({
 export const query = graphql`
   query PostTemplate($id: String!) {
     markdownRemark(id: { eq: $id }) {
+      id
       html
+      tableOfContents(maxDepth: 2)
+      timeToRead
       frontmatter {
         title
+        created(formatString: "MMMM DD, YYYY")
+        updated(formatString: "MMMM DD, YYYY")
+        author
+        tags
+        thumbnail {
+          childImageSharp {
+            gatsbyImageData(
+              layout: FULL_WIDTH
+              quality: 100
+              placeholder: BLURRED
+            )
+          }
+        }
       }
     }
   }
 `;
 
-export const Head: HeadFC<Queries.PostTemplateQuery> = ({ data }) => (
+export const Head: HeadFC<PostTemplateProps> = ({ data }) => (
   <SEO title={data.markdownRemark.frontmatter.title} />
 );
 
