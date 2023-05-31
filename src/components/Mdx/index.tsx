77a5ba7605e1e436244a5d1ae7f38452b77e91f5
Richard Nguyen <richard.ng0616@gmail.com>
2023-05-31 12:19:35 -0700

    Add multiple-level list style

--- src/components/Mdx/index.tsx
+++ src/components/Mdx/index.tsx
@@ -32,11 +32,12 @@ const MdxRenderer: React.FC<Props> = ({ data: { mdx }, children }) => {
           <h1
             id="title"
             className={clsx("", {
-              "text-3xl md:text-4xl lg:text-5xl xl:text-6xl": true,
+              "text-5xl xl:text-6xl": true,
               "text-transparent bg-clip-text bg-gradient-to-r to-sky-200 from-cyan-500":
                 true,
               "font-extrabold gradient lg:font-black": true,
-              "lg:leading-loose tracking-tight lg:tracking-tighter": true,
+              "leading-normal xl:leading-loose tracking-tight lg:tracking-tighter":
+                true,
             })}
           >
             {mdx.frontmatter.title}
