956c575e251e0213219e3b06d35c7fe0a1491f69
Richard Nguyen <richard@richardhnguyen.com>
2023-06-30 07:46:37 -0700

    chore: Add more content button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import clsx from "classnames";
+import { ArrowRightIcon } from "@primer/octicons-react";
 
 import { SEO } from "@components/SEO";
 import TypeWriter from "@components/Typewritter";
 import Articles from "@components/Article";
 import ArrowLink from "@components/Link/ArrowLink";
-import { ArrowRightIcon } from "@primer/octicons-react";
 
 const IndexPage: React.FC = () => {
   return (
@@ -36,11 +36,14 @@ export const Head = () => (
 const MoreContentLink: React.FC = () => {
   return (
     <ArrowLink native href="/posts/">
-      <div className="group flex items-center gap-3 text-sky-500">
+      <div className="group flex items-center gap-3 text-sky-500 mt-16">
         <h3 className="text-5xl font-black">More content</h3>
         <ArrowRightIcon
           size={48}
-          className="opacity-0 -translate-x-4 group-hover:opacity-100 group-hover:translate-x-4 transition-all duration-100"
+          className={clsx("transition-all duration-100", {
+            "opacity-0 -translate-x-4": true,
+            "group-hover:opacity-100 group-hover:translate-x-4": true,
+          })}
         />
       </div>
     </ArrowLink>
