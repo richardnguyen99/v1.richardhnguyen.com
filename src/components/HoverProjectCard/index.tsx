ad3fad02b011240485a9d299cb2c12fad5070fb1
Richard Nguyen <richard@richardhnguyen.com>
2023-07-13 17:43:03 -0700

    feat(about): Responsive project cards

--- src/components/HoverProjectCard/index.tsx
+++ src/components/HoverProjectCard/index.tsx
@@ -58,7 +58,7 @@ const HoverProjectCard: React.FC<Props> = ({
 
   return (
     <div
-      className={clsx("w-full xl:w-[90%]", {
+      className={clsx("w-full xl:w-[90%] overflow-hidden", {
         "mx-auto mt-20 first:mt-10": true,
         "w-full h-full": true,
         "p-12 rounded-[3rem]": true,
@@ -75,7 +75,9 @@ const HoverProjectCard: React.FC<Props> = ({
         <div
           ref={imageRef}
           className={clsx({
-            "w-[320px] h-[400px]": true,
+            "w-[192px] h-[240px]": true,
+            "md:w-[256px] md:h-[320px]": true,
+            "lg:w-[320px] lg:h-[400px]": true,
             "bg-cover relative overflow-hidden": true,
             "rounded-[3rem]": true,
           })}
@@ -84,17 +86,17 @@ const HoverProjectCard: React.FC<Props> = ({
         </div>
         <div
           className={clsx("relative", {
-            "w-[calc(100%-320px)] h-[400px] overflow-hidden lg:ml-16 xl:ml-24 ":
+            "w-[calc(100%-192px)] h-[240px] md:w-[calc(100%-256px)] md:h-[320px] lg:w-[calc(100%-320px)] lg:h-[400px] overflow-hidden ml-12 lg:ml-16 xl:ml-24 ":
               true,
             "flex flex-col": true,
           })}
         >
           <div className="grid grid-cols-[repeat(5,_1fr)_minmax(32px,_auto)] w-full items-center">
-            <h1 className="md:order-1 lg:order-1 md:col-span-5 xl:col-start-1 xl:col-span-3 text-3xl font-black overflow-auto">
+            <h1 className="order-1 col-span-5 xl:col-start-1 xl:col-span-3 text-3xl font-black overflow-auto">
               {title}
             </h1>
 
-            <div className="lg:order-3 xl:order-2 flex lg:col-start-1 lg:col-span-6 xl:col-start-4 xl:col-span-3 xl:col-end-6 items-center gap-3">
+            <div className="order-3 xl:order-2 flex col-start-1 col-span-6 xl:col-start-4 xl:col-span-3 xl:col-end-6 items-center gap-3">
               {externalLink !== "#" && (
                 <a
                   href={externalLink}
@@ -106,7 +108,7 @@ const HoverProjectCard: React.FC<Props> = ({
                 </a>
               )}
             </div>
-            <span className="flex items-center justify-center lg:order-2 xl:order-3 col-span-1 col-end-7 xl:ml-auto text-2xl font-black">
+            <span className="flex items-center justify-center order-2 xl:order-3 col-span-1 col-end-7 xl:ml-auto text-2xl font-black">
               {renderStatus(status)}
             </span>
           </div>
@@ -119,7 +121,9 @@ const HoverProjectCard: React.FC<Props> = ({
                   target="_blank"
                   rel="noreferrer"
                   className={clsx("flex items-center gap-3 relative", {
-                    "text-lg font-light uppercase tracking-[0.25em]": true,
+                    "text-base font-light uppercase tracking-[0.15em]": true,
+                    "lg:text-lg lg:tracking-[0.25em]": true,
+
                     "text-slate-800 dark:text-slate-200": true,
                     "hover:text-slate-950  dark:hover:text-slate-50": true,
 
@@ -138,13 +142,13 @@ const HoverProjectCard: React.FC<Props> = ({
           </div>
         </div>
       </div>
-      <div className="w-fit ml-auto">
-        <div className="flex items-center gap-4 mt-8 w-full after:content-[''] after:block after:w-[120px] after:h-[1px] after:dark:bg-white">
+      <div className="w-fit">
+        <div className="flex items-center gap-4 mt-8 w-full overflow-x-hidden before:content-[''] before:block before:w-[120px] before:h-[1px] before:dark:bg-white">
           {stacks.map((stack, index) => (
             <a
               href="#"
               key={index}
-              className="text-sm uppercase font-light tracking-wider after:content-[','] last-of-type:after:content-['']"
+              className="text-sm uppercase font-light tracking-[0.25em] after:content-[','] last-of-type:after:content-['']"
             >
               {stack}
             </a>
